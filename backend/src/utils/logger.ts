ae184187be2371ca32e3899978ccecfb61fb2fab
Manu Sharma <manu.viswanad@gmail.com>
2025-05-30 21:43:24 +0530

    edited logger

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -5,7 +5,7 @@ const logger = new (winston.Logger)({
     level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
     transports: [
         new (winston.transports.Console)(),
-        new (winston.transports.File)({ filename: 'backend.log' })
+        // new (winston.transports.File)({ filename: 'backend.log' })
     ]
 });
 
