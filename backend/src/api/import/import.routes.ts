79397ee4c656cb237a64b7aa26d5edc567cec01b
Manu Sharma <manu.viswanad@gmail.com>
2025-07-10 17:15:48 +0530

    feat: Update Excel data source for import functionality and add new staging database

--- backend/src/api/import/import.routes.ts
+++ backend/src/api/import/import.routes.ts
@@ -88,7 +88,7 @@ importRoutes.get('/convertExcelToJson', async (req, res, next) => {
   console.log('Directory of this file (__dirname):', __dirname);
 
   try {
-    const data = await excelFileToJson('scrapedata.xlsx', {
+    const data = await excelFileToJson('dataNew.xlsx', {
       sheetNameOrIndex: 0,
       skipBlankRows: true,
       rawValues: true,
